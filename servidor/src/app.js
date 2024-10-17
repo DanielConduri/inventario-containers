88d26764f6ca92fa8dd847ab7dbf26427c72d5d5
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-10-17 00:19:52 -0500

    Configuration 5.4

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -8,11 +8,22 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  //res.header('Access-Control-Allow-Origin', 'https://inventarios.espoch.edu.ec/');
+  res.header('Access-Control-Allow-Origin', 'http://54.91.59.99');
+  
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 const whiteList = [
   //"https://pruebasw.espoch.edu.ec:3011/",
   "http://localhost:4200",
   "https://localhost:4200",
   "https://inventario-espoch.rubenvn.com",
+  "http://54.91.59.99/"
 ];
 
 app.use(
